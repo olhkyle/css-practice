ea62af13f58a6bbb32f1cd618b9ce62ae494e449
kyle <kylekwon96@gmail.com>
2022-02-22 17:31:35 +0900

    20220222(1)

--- profile-card/app.js
+++ profile-card/app.js
@@ -4,15 +4,24 @@ const body = document.querySelector('body');
 const headerLogo = document.querySelector('.header-logo');
 const toggleBtn = document.querySelector('#nav-comp-btn');
 const navBar = document.querySelector('.nav-bar');
+const navBarAnchor1 = document.querySelector('.nav-bar-1 a');
+const navBarAnchor2 = document.querySelector('.nav-bar-2 a');
+const navBarAnchor3 = document.querySelector('.nav-bar-3 a')
+
 
 
 toggleBtn.addEventListener('click', () => {
     navBar.classList.toggle('active');
+    toggleBtn.classList.toggle('clicked');
 })
 
 
 headerLogo.addEventListener('click', () => {
     body.classList.toggle('active');
     headerLogo.classList.toggle('active');
+    toggleBtn.classList.toggle('black');
+    navBarAnchor1.classList.toggle('white');
+    navBarAnchor2.classList.toggle('white');
+    navBarAnchor3.classList.toggle('white');
 })
 
---
+++ profile-card/readme.md
@@ -0,0 +1,20 @@
+# 간단한 프로필 소개 웹페이지 구현하기
+
+#### Desktop 화면
+https://user-images.githubusercontent.com/99726297/155092193-4baae103-4c6b-4ffb-b1ea-aa74e4a210ae.mov
+
+#### 최소 너비 화면
+https://user-images.githubusercontent.com/99726297/155092724-0ff9a0be-3733-4bda-994d-efb88a8190a5.mov
+
+
+### 1. 스택
+    HTML, CSS, Javascript, Bootstrap
+
+### 2. 기능
+1. 좌측 상단의 로고를 클릭하면, 웹페이지의 배경색이 검정색으로 바뀌며, 전반적인 스타일링이 바뀐다.
+2. **Logo**와 **navigation**들에 마우스를 hovering 하면, underline이 좌에서 우로 채워지는 animation 형태로 구현된다.
+3. 각 프로필들에 마우스를 hovering 하면, 배경색이 바뀐다.
+4. 브라우저 화면이 768px보다 작을 때, navigation 항목들이 없어지고, 토글 버튼이 생긴다. 그리고 그 토글 버튼을 클릭하면, 토글 버튼이 회전하면서 navigation 버튼이 나타난다.
+
+### 3. 느낀 점 및 앞으로 구현할 기능
+> <p> 제일 하단의 'Add your profile' 버튼을 클릭하면, 다른 페이지로 이동하여 본인의 프로필을 등록할 수 있는 폼을 만들어 연동해 본다. </p>
